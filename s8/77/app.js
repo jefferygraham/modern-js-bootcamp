51e75ae7c8f47b598908a498cf866d51c3e5490f
jeff <jeffery.lemar.graham@gmail.com>
2022-01-09 19:45:20 -0500

    the world of loops

---
+++ s8/77/app.js
@@ -0,0 +1,13 @@
+function grumpus() {
+  console.log('Ugh...you again?!');
+  console.log('For the last time...');
+  console.log('LEAVE ME ALONE!!!');
+}
+
+grumpus();
+grumpus();
+grumpus();
+
+for (let i = 0; i < 50; i++) {
+  grumpus();
+}
